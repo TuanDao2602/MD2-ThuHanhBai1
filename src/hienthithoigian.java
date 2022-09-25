e4ec35658bf0d1e7eb8b8240d6a9a5fbc5e0c095
TuanDao2602 <tuanktktcn@gmail.com>
2022-09-25 16:52:04 +0700

    thuchanh1

---
+++ src/hienthithoigian.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class hienthithoigian {
+    public static void main(String[] args){
+        Date date = new Date();
+        System.out.println("thời gian hiện tại là :" + date);
+
+    }
+}
